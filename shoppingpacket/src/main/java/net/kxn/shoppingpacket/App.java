87f11103bf241d4af935b3eb9328ac91b1710dfc
shubham gupta <shubham.jnct57@gmail.com>
2017-08-15 21:02:51 +0530

    my project has been establish successfully

---
+++ shoppingpacket/src/main/java/net/kxn/shoppingpacket/App.java
@@ -0,0 +1,13 @@
+package net.kxn.shoppingpacket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
